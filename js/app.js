f0965443a645ae17c1fed72166e22adc349b08fe
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2022-12-19 20:01:51 +0630

    render

---
+++ js/app.js
@@ -0,0 +1,121 @@
+const $ = (selector) => document.querySelector(selector);
+const createEl = (el) => document.createElement(el);
+
+let activeIndex = 0;
+
+const projects = [
+  {
+    logo: "olive-go.jpg",
+    projectName: "Olive Go - Home Improvement",
+    projectLink: "https://olivego.com/",
+    projectBrief: `Customers will be able to shop from anywhere across the nation for a
+      full range of home improvement products with installation, repair, and
+      maintenance services that complement every product available on the
+      platform.`,
+    approaches: [],
+  },
+  {
+    logo: "midas.png",
+    projectName: "Midascreatives",
+    projectLink: "https://midascreatives.com.mm/",
+    projectBrief: `Midas Creatives is a creative agency which is established and founded in Singapore. They help companies grow business and revenue. They deliver strong branding identity through marketing communications across all media.`,
+    approaches: [],
+  },
+  {
+    logo: "player.ico",
+    projectName: "My Playlist",
+    projectLink: "https://myplaylist.vercel.app/",
+    projectBrief: `A music player with beautifully animated particle effects. The
+      lyric is translated into English.`,
+    techBrief: `I created this player using React + Redux. Styled-componets is used for the styling.`,
+    approaches: [],
+  },
+
+  {
+    logo: "venuslab.svg",
+    projectName: "Venus Lab Website Clone",
+    projectLink: "https://venuslab-clone.vercel.app/",
+    projectBrief: `This is the cloned website of https://venuslab.co.<br /> I created this website as part of the interview process when I applied for the job.`,
+    techBrief: `NextJS - Styled Components`,
+    approaches: [],
+  },
+
+  {
+    logo: "codepen.svg",
+    projectName: "My Pens on CodePen.io",
+    projectLink: "https://codepen.io/yanmoenaing118",
+    projectBrief: `Beautiful pens I created to learn new things.`,
+    techBrief: `CSS, SCSS, Animation, DOM`,
+    approaches: [],
+  },
+];
+
+function setUpProject() {
+  const projectsContainer = document.getElementById("projects");
+  const projectEl = document.createElement("div");
+  const projectImgWrapper = document.createElement("div");
+  const projectImg = document.createElement("img");
+  const projectTitle = document.createElement("h2");
+  const projectUrl = document.createElement("a");
+  const projectAbout = document.createElement("p");
+  const projectApproaches = document.createElement("div");
+  const tags = document.createElement("div");
+
+  return {
+    state: {
+      activeIndex: 0,
+    },
+    setState(newState) {
+        console.log(newState);
+      this.state = { ...newState };
+      this.render();
+    },
+    render() {
+      const child = projectsContainer.querySelector("project");
+      if (child) {
+        projectsContainer.removeChild(child);
+      }
+      projectImgWrapper.appendChild(projectImg);
+      projectEl.appendChild(projectImgWrapper);
+      projectEl.appendChild(projectTitle);
+      projectEl.appendChild(projectUrl);
+      projectEl.appendChild(projectAbout);
+      projectApproaches.appendChild(tags);
+      projectEl.appendChild(projectApproaches);
+      projectsContainer.appendChild(projectEl);
+
+      projectEl.classList.add("project");
+      projectImgWrapper.classList.add("project__img");
+      projectTitle.classList.add("project__title");
+      projectUrl.classList.add("project__url");
+      projectAbout.classList.add("project__about");
+      projectApproaches.classList.add("project__tech");
+      tags.classList.add("tag__list");
+
+      projectImg.src =
+        "https://yanmoenaing118.github.io/assets/" +
+        projects[this.state.activeIndex].logo;
+      projectTitle.textContent = projects[this.state.activeIndex].projectName;
+      projectUrl.textContent = projects[this.state.activeIndex].projectLink;
+      projectUrl.href = projects[this.state.activeIndex].projectLink;
+      projectAbout.textContent = projects[this.state.activeIndex].projectBrief;
+    },
+  };
+}
+
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+console.log(prevBtn);
+console.log(nextBtn);
+
+const project = setUpProject();
+project.render();
+
+prevBtn.addEventListener("click", () => {
+    console.log('hi')
+    project.setState({ activeIndex: ++activeIndex})
+})
+
+nextBtn.addEventListener("click", () => {
+    project.setState({ activeIndex: ++activeIndex })
+})
